4067865be070d63daa8fdb185f413b37ea98f5c4
GustavoCostaAM <gustavo.costa.m949@gmail.com>
2025-09-27 23:26:17 -0300

    configurações de dotenv feita

--- src/main/java/com/scaneia/ScaneiaServlet/Config/EnvConfig.java
+++ src/main/java/com/scaneia/ScaneiaServlet/Config/EnvConfig.java
@@ -1,4 +1,11 @@
 package com.scaneia.ScaneiaServlet.Config;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 public class EnvConfig {
+    private final static Dotenv dotenv = Dotenv.load();
+
+    public static String getEnv(String chave){
+        return dotenv.get(chave);
+    }
 }
