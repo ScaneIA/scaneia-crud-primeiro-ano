599e92a306b41e0705ce41b2284cf503808c810f
GustavoCostaAM <gustavo.costa.m949@gmail.com>
2025-10-15 08:48:18 -0300

    http session feita

--- src/main/java/com/scaneia/ScaneiaServlet/Servlet/AreaRH/AreaRHServlet.java
+++ src/main/java/com/scaneia/ScaneiaServlet/Servlet/AreaRH/AreaRHServlet.java
@@ -11,7 +11,6 @@ import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
 @WebServlet(name = "areaRH", value = "/areaRH")
