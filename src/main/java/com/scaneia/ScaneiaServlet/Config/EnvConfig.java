aa0ac42d03b4e8c9d93aa0ec659cde65603d9adc
GustavoCostaAM <gustavo.costa.m949@gmail.com>
2025-10-26 13:58:45 -0300

    removendo dotenv e gerenciando .env pelo sistema

--- src/main/java/com/scaneia/ScaneiaServlet/Config/EnvConfig.java
+++ src/main/java/com/scaneia/ScaneiaServlet/Config/EnvConfig.java
@@ -1,11 +1,7 @@
 package com.scaneia.ScaneiaServlet.Config;
 
-import io.github.cdimascio.dotenv.Dotenv;
-
 public class EnvConfig {
-    private final static Dotenv dotenv = Dotenv.load();
-
     public static String getEnv(String chave){
-        return dotenv.get(chave);
+        return System.getenv(chave);
     }
 }
